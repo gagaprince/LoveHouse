7c01669e9d6f80788a09bf6b0375601ce63aba35
gagaprince <wangzidongprince@163.com>
2015-08-12 14:12:44 +0800

    shareTest

--- src/main/webapp/fe/js/test/index.js
+++ src/main/webapp/fe/js/test/index.js
@@ -5,5 +5,7 @@ function initPage(){
     initShare();
 }
 function initShare(){
-    wxBridge.setImgUrl("https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2871394307,1854683926&fm=116&gp=0.jpg");
+    wxBridge.setImgUrl("http://cgzp.cdn.novorunning.com/second/images/big_icon.jpg");
+    wxBridge.setLink("http://second.shenzhankai.com/second/index.html");
+    wxBridge.setTitle("我按出了10个1秒，一个终极大奖都木有，这个是骗人滴！")
 }
